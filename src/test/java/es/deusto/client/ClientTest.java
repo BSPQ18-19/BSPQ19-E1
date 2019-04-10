14ae0a6850c9594f286c0859a2866a7202814794
Aleksander Bobiński <aleksanderbobinski@interia.eu>
2019-04-10 11:52:38 +0200

    Add support for tests and documentation
    Added support for javadoc, junit, jacoco, surefire, pmd in pom.xml

---
+++ src/test/java/es/deusto/client/ClientTest.java
@@ -0,0 +1,21 @@
+package es.deusto.client;
+
+import es.deusto.server.IServer;
+import org.junit.After;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertTrue;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class ClientTest {
+
+    @Test
+    public void testConnectionToServer() {
+	assertTrue(true);
+    }
+}
