94d204add87cbb770c06d4840375b57e026ca34e
Pablo <pabloalonso998@gmail.com>
2019-05-18 18:19:40 +0200

    Added some methods for movie searching
    #BSPQ19E1-50 To Verify
    #BSPQ19E1-50 add work 40m development

--- src/main/java/es/deusto/server/IServer.java
+++ src/main/java/es/deusto/server/IServer.java
@@ -1,6 +1,6 @@
 package es.deusto.server;
 
-import es.deusto.server.data.UserDTO;
+import es.deusto.server.data.*;
 import es.deusto.server.jdo.Movie;
 
 import java.rmi.Remote;
@@ -11,6 +11,9 @@ public interface IServer extends Remote {
 	void addMovie(String title, String director, List<String> cast) throws RemoteException;
 	void deleteMovie(String title) throws RemoteException;
 	Movie getMovie(String title) throws RemoteException;
+	List<MovieDTO> searchMovies(String query) throws RemoteException;
+	List<SessionDTO> getSessionsForMovie(MovieDTO mdto) throws RemoteException;
+	List<MovieDTO> getMoviesForDay(int year, int month, int day) throws RemoteException;
 	UserDTO logIn(String email, String password) throws RemoteException;
 	boolean logOut(UserDTO user) throws RemoteException;
 }
--- src/main/java/es/deusto/server/Server.java
+++ src/main/java/es/deusto/server/Server.java
@@ -3,17 +3,26 @@ package es.deusto.server;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import es.deusto.server.data.MovieDTO;
+import es.deusto.server.data.SessionDTO;
 import es.deusto.server.data.UserDTO;
 import es.deusto.server.jdo.DAO;
 import es.deusto.server.jdo.Movie;
+import es.deusto.server.jdo.Session;
 import es.deusto.server.jdo.User;
 
 public class Server extends UnicastRemoteObject implements IServer {
 
 	private static final long serialVersionUID = 1L;
 	private int cont = 0;
+	// This probably isn't a very good idea as all clients
+	// will be using the same DAO instance so transactions may collide
+	// But it can be easily changed
 	private DAO dao;
 
 	public Server() throws RemoteException {
@@ -52,23 +61,67 @@ public class Server extends UnicastRemoteObject implements IServer {
 		return userDTO;
 	}
 
+	// We don't need this I think
 	public boolean logOut(UserDTO user) throws RemoteException {
 		return false;
 	}
 
+
+	@Override
+	public List<MovieDTO> searchMovies(String query) throws RemoteException {
+		dao.begin();
+		List<Movie> movies = dao.searchMovieByTitle(query);
+		List<MovieDTO> moviesDTO = new ArrayList<>(movies.size());
+		for(Movie m : movies) {
+			moviesDTO.add(new MovieDTO(m));
+		}
+		dao.end();
+		return moviesDTO;
+	}
+
+	@Override
+	public List<SessionDTO> getSessionsForMovie(MovieDTO mdto) throws RemoteException {
+		dao.begin();
+		Movie movie = dao.getMovie(mdto.id);
+		List<Session> sessions = movie.getSessions();
+		List<SessionDTO> sessionsDTO = new ArrayList<>(sessions.size());
+		for (Session s : sessions) {
+			sessionsDTO.add(new SessionDTO(s));
+		}
+		dao.end();
+		return sessionsDTO;
+	}
+
+	@Override
+	public List<MovieDTO> getMoviesForDay(int year, int month, int day) throws RemoteException {
+		dao.begin();
+		List<Session> sessions = dao.getSessions(year, month, day);
+		// This should make repeated movies not save twice
+		Set<Movie> moviesset = new HashSet<>();
+		for (Session s : sessions) {
+			moviesset.add(s.getMovie());
+		}
+		List<MovieDTO> movieslist = new ArrayList<>(moviesset.size());
+		for (Movie m : moviesset) {
+			movieslist.add(new MovieDTO(m));
+		}
+		dao.end();
+		return movieslist;
+	}
+	
 	public static void main(String[] args) {
 		if (args.length != 3) {
 			System.out.println("How to invoke: java [policy] [codebase] Server.Server [host] [port] [server]");
 			System.exit(0);
 		}
-
+		
 		if (System.getSecurityManager() == null) {
 			System.setSecurityManager(new SecurityManager());
 		}
-
+		
 		System.out.println(args);
 		String name = "//" + args[0] + ":" + args[1] + "/" + args[2];
-
+		
 		try {
 			IServer objServer = new Server();
 			Naming.rebind(name, objServer);
